425751a16f085da933994b854d79b50284ae39a2
calvintruong26 <calvintruong26@live.com>
2017-11-29 17:56:53 -0800

    add pagination

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  FEAT_POSTS_SIZE: 2,
+  BLOG_INDEX_SIZE: 6,
+  RECENT_POSTS_SIZE: 10
+};
--- server.js
+++ server.js
@@ -3,14 +3,16 @@ var app = express();
 var mongoose = require("mongoose");
 var Post = require("./models/post");
 var seedDB = require("./seed");
+var config = require("./config");
 
 
 mongoose.connect("mongodb://localhost/atravelershigh");
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 
+// Get info on most recent posts
 app.get('/api/posts/recent', function(req, res) {
-    Post.find({}).sort({'date': -1}).limit(10).exec(function(err, posts) {
+    Post.find({}).sort({'date': -1}).limit(config.RECENT_POSTS_SIZE).exec(function(err, posts) {
         if (err) {
           console.log(err);
         }
@@ -20,6 +22,7 @@ app.get('/api/posts/recent', function(req, res) {
     });
 });
 
+// Get info on specific post
 app.get('/api/posts/:id', function(req, res) {
     var num = req.params.id;
    Post.find({_id: num}, function (err, foundBlog){
@@ -33,10 +36,13 @@ app.get('/api/posts/:id', function(req, res) {
 
 });
 
+// INDEX
 app.get('/', function(req, res){
-  Post.find({}).sort({'date': -1}).limit(2).exec(function(err, foundFeaturedBlogs) {
+  // Fetch two most recent posts for featured section
+  Post.find({}).sort({'date': -1}).limit(config.FEAT_POSTS_SIZE).exec(function(err, foundFeaturedBlogs) {
     if (err) {
       console.log(err);
+      res.redirect('/');
     }
     else {
       res.render('./main/index', {featuredBlogs: foundFeaturedBlogs});
@@ -44,16 +50,45 @@ app.get('/', function(req, res){
   });
 });
 
+// TRAVEL
 app.get('/travel', function(req, res){
-  Post.find({category: 'Travel'}).sort({'date': -1}).limit(6).exec(function(err, foundTravelPosts){
-    if (err) {
-      console.log(err);
-      res.redirect('/');
-    }
-    else {
-      res.render('./travel/index', {travelPosts: foundTravelPosts});
-    }
-  });
+  // Get page query
+  var page = req.query.page || 1;
+  if (page < 1 || isNaN(page)) {
+    console.log("Page " + page + " doesn't exist...");
+    return res.render("./main/404")
+  }
+  else {
+    // Retrieve page results
+    page = parseInt(page);
+    Post.find({category: 'Travel'})
+        .sort({'date': -1})
+        .exec(function(err, foundTravelPosts){
+      if (err) {
+        console.log(err);
+        res.redirect('/');
+      }
+      else {
+        var numResults = foundTravelPosts.length;
+        var firstResult = (page - 1) * config.BLOG_INDEX_SIZE;
+        foundTravelPosts = foundTravelPosts.slice(firstResult, firstResult + config.BLOG_INDEX_SIZE);
+
+        if (numResults > 0 && foundTravelPosts.length < 1) {
+          console.log("Page " + page + " doesn't exist...");
+          return res.render("./main/404");
+        }
+        else {
+            res.render('./travel/index',
+            {
+              travelPosts: foundTravelPosts,
+              page: page,
+              numResults: numResults,
+              PAGE_SIZE: config.BLOG_INDEX_SIZE
+            });
+        }
+      }
+    });
+  }
 });
 
 app.get('/travel/:id', function(req, res) {
